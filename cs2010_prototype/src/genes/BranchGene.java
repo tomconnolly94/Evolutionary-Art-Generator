9ccaf5d8fc33e148f288fa4d4bb7f7074e1358b8
Tom Connolly <tom.connolly2511@gmail.com>
2014-12-16 14:06:48 +0000

    Update BranchGene.java

--- cs2010_prototype/src/genes/BranchGene.java
+++ cs2010_prototype/src/genes/BranchGene.java
@@ -2,6 +2,10 @@ package genes;
 /**
  * Class to model a Branch gene. This determines how many branches will be generated from the centre,
  * then at the end of each branch if the Chain gene permits it.
+ *
+ * @author Tom Connolly
+ * @version 22 Nov 2014
+ */
  */
 public class BranchGene extends Gene
 {	
