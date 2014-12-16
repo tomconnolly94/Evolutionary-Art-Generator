095d2cead5a3dc5d0c4dfd61eaa33ec0fc568b40
Tom Connolly <tom.connolly2511@gmail.com>
2014-12-16 14:08:09 +0000

    Update BiomorphCreator.java

--- cs2010_prototype/src/program/BiomorphCreator.java
+++ cs2010_prototype/src/program/BiomorphCreator.java
@@ -2,6 +2,9 @@ package program;
 import java.util.Random;
 /**
  * Factory class to generate a random biomorph.
+ * 
+ * @author Tom Connolly
+ * @version 22 Nov 2014
  */
 public class BiomorphCreator
 {
