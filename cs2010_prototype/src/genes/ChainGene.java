f1a74aebf3c5bc1b78585f70169002dbf40c3fde
Tom Connolly <tom.connolly2511@gmail.com>
2014-12-16 14:07:11 +0000

    Update ChainGene.java

--- cs2010_prototype/src/genes/ChainGene.java
+++ cs2010_prototype/src/genes/ChainGene.java
@@ -1,6 +1,9 @@
 package genes;
 /**
  * Class to model a Chain gene. This determines how many times the biomorph will branch.
+ * 
+ * @author Tom Connolly
+ * @version 22 Nov 2014
  */
 public class ChainGene extends Gene
 {	
