91443e5774d0d266aa5dbe43ec7a10815ed31e5f
Tom Connolly <tom.connolly2511@gmail.com>
2015-02-23 16:20:39 +0000

    Empty Save class added.

---
+++ cs2010_prototype/src/program/Save.java
@@ -0,0 +1,30 @@
+package program;
+
+public class Save {
+
+	
+	public Save(){
+		
+	}
+	
+	public void changeSaveDestination(){
+		
+	}
+	
+	public void saveToPNG(){
+		
+	}
+	
+	public void saveToJPG(){
+		
+	}
+	
+	public void saveToPDF(){
+		
+	}
+	
+	public void serialiseBiomorph(){
+		
+	}
+	
+}
