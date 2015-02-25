83490747d075fc9d65885725cf73a151073904dc
Tom Connolly <tom.connolly2511@gmail.com>
2015-02-25 11:25:07 +0000

    Split Controller into Drawer(controls all activities to do with setting up the LWJGL window and selecting a biomorph) and BiomorphManager(controls all activities concerning handling and storage of biomorphs).

---
+++ cs2010_prototype/src/program/BiomorphManager.java
@@ -0,0 +1,71 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import evolution.*;
+
+public class BiomorphManager {
+
+	private ArrayList<Biomorph> biomorphCollection = new ArrayList<Biomorph>();
+	int[] perfectValues = {5,5,5,5,5,5,5,5,5,5,5};
+	
+	public BiomorphManager(){}
+	/**
+	 * Creates a biomorph and adds it to the list of biomorphs.
+	 */
+	public Biomorph createAndAdd()
+	{
+		BiomorphCreator bc = new BiomorphCreator();
+		Biomorph biomorph = bc.createBiomorph();
+		biomorphCollection.add(biomorph);
+		return biomorph;
+	}
+	/**
+	 * @return A random biomorph from the list
+	 */
+	public Biomorph getRandomBiomorph()
+	{	
+		Random rand = new Random();
+		return biomorphCollection.get(rand.nextInt(biomorphCollection.size() - 1));
+	}
+	/**
+	 * Retrieves a specific biomorph by its index number in the list.
+	 * @param index The index number
+	 * @return The biomorph corresponding to the given index number
+	 */
+	public Biomorph getSpecific(int index)
+	{
+		return biomorphCollection.get(index);
+	}
+	/**
+	 * Removes a biomorph corresponding to the given index number.
+	 * @param index The index number
+	 */
+	public void remove(int index)
+	{
+		biomorphCollection.remove(index);
+	}
+	/** @return How many biomorphs are currently held in the biomorphCollection*/
+	public int getSize(){
+		return biomorphCollection.size();
+	}
+	public void addSpecific(Biomorph b){
+		biomorphCollection.add(b);
+	}
+	/** Two new biomorphs are created and immediately added to the biomorphCollection, then they are given to the
+	 *  EvolveBlend object to be evolved using its specific averaging algorithm. The resulting biomorph is then 
+	 *  added to the array at index 2 so that the code on line 113 will find it. */
+	public void loadBiomorphsWithEvBle(){
+		EvolveBlend eb = new EvolveBlend(createAndAdd(), createAndAdd());
+		addSpecific(eb.evolve());
+	}
+	/** Two new biomorphs are created and immediately added to the biomorphCollection, then they are 
+	 *  given to the EvolveBlend object to be evolved using its specific 'closest value wins' algorithm. 
+	 *  The resulting biomorph is then added to the array at index 2 so that the code on line 113 will 
+	 *  find it. */
+	public void loadBiomorphsWithEvClo(){
+		EvolveClosest ec = new EvolveClosest(createAndAdd(), createAndAdd(), perfectValues);
+		addSpecific(ec.evolve());
+	}
+}
---
+++ cs2010_prototype/src/program/Drawer.java
@@ -0,0 +1,67 @@
+package program;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+public class Drawer {
+
+	/**
+	 * The main method for this application. Takes care of selecting and drawing a Biomorph.
+	 */
+	public static void main(String[] args)
+	{
+		boolean quit = false;
+		boolean keystop = false;
+		float aspect;
+		//Set display to 1024x768, windowed
+		DisplayMode mode = new DisplayMode(1024, 768);
+		try
+		{
+			Display.setDisplayMode(mode);
+			Display.create();
+			Display.setTitle("Biomorph Test");
+		}
+		catch (Exception e) {}
+		//Set the display's aspect ratio (in this case, 4:3)
+		aspect = (float)mode.getWidth() / (float)mode.getHeight();
+		//Set projection boundaries for OpenGL drawing
+		GL11.glOrtho(-50.0f * aspect, 50.0f * aspect, -50.0f, 50.0f, 0.0f, 1.0f);
+		BiomorphManager bm = new BiomorphManager();
+		/* A load biomorphs method is called to use EvolveBlend class to evolve the Biomorph that
+		 * will be displayed.*/
+		bm.loadBiomorphsWithEvBle();
+		while (quit == false)
+		{
+			//Pressing Enter will generate a new biomorph.
+			if (Keyboard.isKeyDown(Keyboard.KEY_RETURN))
+			{
+				if (keystop == false)
+				{
+					//biomorph collection is emptied.
+					for(int i=0; i<bm.getSize(); i++){
+						bm.remove(i);
+					}
+					/* A load biomorphs method is called to use EvolveClosest class to evolve the 
+					 * Biomorph that will be displayed.*/
+					bm.loadBiomorphsWithEvClo();
+				}
+				keystop = true;
+			}
+			
+			//This prevents repeated creations of biomorphs while the Enter key is held down.
+			if (Keyboard.isKeyDown(Keyboard.KEY_RETURN) == false) keystop = false;
+			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) quit = true;
+	        if (Display.isCloseRequested()) quit = true;
+	        //Clear screen and draw biomorph
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+			//Biomorph is extracted from array at index 2, this is where the evolved Biomorph was placed earlier.
+			bm.getSpecific(2).draw();
+			Display.update();
+			//Limit to 60fps to save CPU usage
+			Display.sync(60);
+		}
+		Display.destroy();
+	}
+}
