afeb93a68603ac909fb99449e438e406265bbc31
dmd1337 <dmd1337@hotmail.co.uk>
2015-02-25 20:06:07 +0000

    Vertex and Limbs

--- cs2010_prototype/src/genes/Gene.java
+++ cs2010_prototype/src/genes/Gene.java
@@ -7,7 +7,7 @@ package genes;
 public abstract class Gene
 {
 	private int value;
-	private String type;
+	private String type; //The value of type is never set in any of the gene classes. It would appear it is not actually needed.
 	public Gene(int value)
 	{
 		this.value = value;
