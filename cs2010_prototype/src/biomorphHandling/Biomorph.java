bc25639db553d0bc3c4c64544068c1f3f3b4246f
Tom Connolly <tom.connolly2511@gmail.com>
2015-03-05 13:49:35 +0000

    lengthIncrement gene implemented.

--- cs2010_prototype/src/biomorphHandling/Biomorph.java
+++ cs2010_prototype/src/biomorphHandling/Biomorph.java
@@ -67,7 +67,7 @@ public class Biomorph
 			// This allows the distribution of branches to be uniform.
 			GL11.glRotatef((float) b * (360.0f / genes[0].getValue()), 0.0f, 0.0f, 1.0f);
 			/* code to update length value using length Increment  TODO: Uncomment when needed.*/
-			// length = (genes[7].getValue() + (limbCount*genes[8].getValue()));
+			length = (genes[7].getValue() + (limbCount*genes[8].getValue()));
 			new CuboidLimb(length, genes[9].getValue(), genes[3].getValue(), genes[4].getValue(), genes[5].getValue()).draw();
 			GL11.glPopMatrix();
 			limbCount++;
