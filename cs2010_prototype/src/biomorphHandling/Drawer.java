3fee30938b59b9e6183285a712ac183d0d776b69
dmd1337 <dmd1337@hotmail.co.uk>
2015-03-12 21:01:22 +0000

    Latitude and Longitude implemented
    This took way too long.

--- cs2010_prototype/src/biomorphHandling/Drawer.java
+++ cs2010_prototype/src/biomorphHandling/Drawer.java
@@ -3,6 +3,7 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
 /**
  * Class to handle the drawing and displaying of Biomorphs.
  * @author Tom Connolly, Jack Taylor, Charandeep Rai.
@@ -19,8 +20,8 @@ public class Drawer
 		boolean quit = false;
 		boolean keystop = false;
 		float aspect = 0;
-		float x = 0.0f;
-		float z = 0.0f;
+		float lat = 0.0f;
+		float lon = 0.0f;
 		// Set display to 1024x768, windowed
 		DisplayMode mode = new DisplayMode(1024, 768);
 		try
@@ -35,7 +36,7 @@ public class Drawer
 		// Set the display's aspect ratio (in this case, 4:3)
 		aspect = (float) mode.getWidth() / (float) mode.getHeight();
 		// Set projection boundaries for OpenGL drawing
-		GL11.glOrtho(-50.0f * aspect, 50.0f * aspect, -50.0f, 50.0f, -50.0f, 50.0f);
+		GL11.glOrtho(-80.0f * aspect, 80.0f * aspect, -80.0f, 80.0f, -80.0f, 80.0f);
 		BiomorphManager bm = new BiomorphManager();
 		//Integer to store runCount for auto-run feature.
 		int i = 0;
@@ -52,18 +53,15 @@ public class Drawer
 			// is held down.
 			if (Keyboard.isKeyDown(Keyboard.KEY_RETURN) == false) keystop = false;
 			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) quit = true;
-			if (Keyboard.isKeyDown(Keyboard.KEY_UP)) x += 1.0f;
-			if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) x -= 1.0f;
-			if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) z -= 1.0f;
-			if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) z += 1.0f;
+			if (Keyboard.isKeyDown(Keyboard.KEY_UP)) if (lat < 90.0f) lat += 2.0f;
+			if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) if (lat > -90.0f) lat -= 2.0f;
+			if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) lon -= 2.0f;
+			if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) lon += 2.0f;
 			if (Display.isCloseRequested()) quit = true;
 			// Clear screen and draw biomorph
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-			// locates and draws biomorph depending on which is chosen to be
-			// drawn by random above
 			GL11.glPushMatrix();
-				GL11.glRotatef(x, 1.0f, 0.0f, 0.0f);
-				GL11.glRotatef(z, 0.0f, 0.0f, 1.0f);
+				GLU.gluLookAt((float)Math.cos(Math.toRadians(lat)) * (float)Math.cos(Math.toRadians(lon)), (float)Math.sin(Math.toRadians(lat)), (float)Math.cos(Math.toRadians(lat)) * (float)Math.sin(Math.toRadians(lon)), 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 				bm.getSpecific(0).draw();
 			GL11.glPopMatrix();
 			Display.update();
