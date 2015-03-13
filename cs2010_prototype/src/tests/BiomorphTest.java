9cf18a99b5a3620fa4ee77d48eabd669f18c0fb4
Tom Connolly <tom.connolly2511@gmail.com>
2015-03-13 13:23:41 +0000

    test class working

--- cs2010_prototype/src/tests/BiomorphTest.java
+++ cs2010_prototype/src/tests/BiomorphTest.java
@@ -11,30 +11,22 @@ import biomorphHandling.*;
 public class BiomorphTest
 {
 	private Biomorph biomorph;
-	private Gene[] genes;
+	private int[] values = {2,2,4,100,100,100,5,2,3,1};
 	
 	@Before //pre-conditions
 	public void setUp()
 	{
 		biomorph = new Biomorph(2,2,4,100,100,100,5,2,3,1);
 		
-		genes[0] = new Branch(2, "Branch");
-		genes[1] = new BranchIncrement(2, "Branch Increment");
-		genes[2] = new Chain(4, "Chain");
-		genes[3] = new Color(100, "Color Red");
-		genes[4] = new Color(100, "Color Green");
-		genes[5] = new Color(100, "Color Blue");
-		genes[6] = new Length(5, "Length");
-		genes[7] = new LengthIncrement(2, "Length Increment");
-		genes[8] = new Thickness(3, "Thickness");
-		genes[9] = new ThicknessIncrement(1, "Thickness Increment");
+		
 	}
 	
 	@Test //tests
 	public void test()
 	{
-		assertEquals(biomorph.getGenes(), genes );
-		
+		for(int i=0; i<biomorph.getGenes().length; i++){
+		assertEquals(biomorph.getGenes()[i].getValue(), values[i]);
+		}
 	}
 	
 	@After //post conditions
