719e692bdb062db9bf070c97b98718fb44b971f9
Charandeep <charandeep.rai@gmail.com>
2015-03-14 01:23:50 +0000

    Brand new filemenu system

--- cs2010_prototype/src/gui/FileMenu.java
+++ cs2010_prototype/src/gui/FileMenu.java
@@ -6,6 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -14,107 +18,117 @@ import javax.swing.border.EmptyBorder;
  * The File Menu element of the program
  * @author Charandeep Rai
  */
+public class FileMenu {
 
-public class FileMenu{
-	
-	
-
-//Creation of the frame for the file system.
-	public static JFrame fileFrame;
-	private static JButton newButton;
-	private static JButton saveButton;
-	private static JButton openButton;
-	
-
-public FileMenu() {
+	//public static JFrame fileFrame;
 
 
-	
-// Padding out the borders with blank spaces	
-	final int blankSpace = 1;
-	
-// Creating components
-	
-	//Button to create a new Biomorph
-	JButton newButton = new JButton();
-	JButton saveButton = new JButton();
-	JButton openButton = new JButton();
-	
-	//Set the properties
-	newButton.setText("New");
-	newButton.setToolTipText("Creates a new Biomorph");
-	
-	saveButton.setText("Save");
-	saveButton.setToolTipText("Saves the created Biomorph");
-	
-	openButton.setText("Open");
-	openButton.setToolTipText("Open up a previously created Biomorph");
-	
-// Creating containers for the components
-	fileFrame = new JFrame();
-	
-	JPanel newPanel = new JPanel();
-	JPanel savePanel = new JPanel();
-	JPanel openPanel = new JPanel();
-	
-// Specifying the layout used
-	
-	fileFrame.setLayout(new BorderLayout());
-	
-	((JPanel)fileFrame.getContentPane()).setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	
-	newPanel.setLayout(new BorderLayout());
-	newPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	
-	savePanel.setLayout(new BorderLayout());
-	savePanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	
-	openPanel.setLayout(new BorderLayout());
-	openPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));;
+	private JMenuBar createMenuBar(String name, int depth) {
+		JMenuBar menubar= new JMenuBar();
+		//JMenuBar openmenubar= new JMenuBar();
+		menubar.add(createNewMenu("New", 4));
+		menubar.add(createSaveMenu("Save", 1));
+		menubar.add(createOpenMenu("Open", 2));
+		menubar.add(createPrintMenu("Print", 3));
 
-// Adding the components to the containers
-	
-	newPanel.add(newButton, BorderLayout.CENTER);
-	savePanel.add(saveButton, BorderLayout.CENTER);
-	openPanel.add(openButton, BorderLayout.CENTER);
-	
-	fileFrame.add(newPanel, BorderLayout.WEST);
-	fileFrame.add(savePanel, BorderLayout.CENTER);
-	fileFrame.add(openPanel, BorderLayout.EAST);
-	
-// Adding the action listens for the buttons
-	
-	newButton.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent n){
-			//Needs Completing
+		return menubar;
+	}
+
+	private JMenu createSaveMenu(String savename, int menudepth){
+		JMenu savemenu = new JMenu(savename);
+		for (int i = 0; i < 1; i++){
+			savemenu.add(createSaveMenuItem("Save as PDF"));
+			savemenu.add(createSaveMenuItem("Save as JPEG"));
+			savemenu.add(createSaveMenuItem("Save as TextFile"));
 		}
-	});
-	
-	saveButton.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent s){
-			//Needs Completing
+		return savemenu;
 	}
-	});
-	
-	openButton.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent o){
-			//Needs Completing
-	}
-	});
 
-// Packing and displaying the GUI
+
+	private JMenu createOpenMenu(String name, int menudepth){
+		JMenu openmenu = new JMenu(name);
+		for (int i = 0; i<1; i++){
+			openmenu.add(createOpenMenuItem("Open from TextFile"));
+		}
+		return openmenu;
+
+	}
+
+	private JMenu createPrintMenu(String name, int menudepth){
+		JMenu printmenu = new JMenu(name);
+		for (int i = 0; i<1; i++){
+			printmenu.add(createPrintMenuItem("Print Biomorph"));
+		}
+		return printmenu;
+	}
+
+	private JMenu createNewMenu(String name, int menudepth){
+		JMenu newmenu = new JMenu(name);
+		for (int i = 0; i<1; i++){
+			newmenu.add(createNewMenuItem("Create New Biomorph"));
+		}
+		return newmenu;
+	}
+
+
+	private JMenuItem createOpenMenuItem(String name) {
+		JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(jMenuItem, "Successfully pressed a menu item");
+			}
+		});
+		return jMenuItem;
+	}
+
+
+	private JMenuItem createSaveMenuItem(String savebutton) {
+		JMenuItem jMenuItem = new JMenuItem(savebutton);
+		
+		jMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent s) {
+				JOptionPane.showMessageDialog(jMenuItem, "Saved!");
+			}
+		});
+		return jMenuItem;
+	}
 	
-	fileFrame.pack();
-	fileFrame.setVisible(true);
+	private JMenuItem createNewMenuItem(String newbutton) {
+		JMenuItem jMenuItem = new JMenuItem(newbutton);
+		
+		jMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent n) {
+				JOptionPane.showMessageDialog(jMenuItem, "New!");
+			
+			}
+			});
+		return jMenuItem;
+	}
+	
+	private JMenuItem createPrintMenuItem(String printbutton) {
+		JMenuItem jMenuItem = new JMenuItem(printbutton);
+		
+		jMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent n) {
+				JOptionPane.showMessageDialog(jMenuItem, "Printed!");
+			
+			}
+			});
+		return jMenuItem;
+	}
+
+	protected void intialise() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(createMenuBar("Save", 1));
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new FileMenu().intialise();
+	}
 }
-	public static void main(String[] args)
-	{
-		FileMenu gm = new FileMenu();
-	}	
-	
-	
-	
-	
-	
-	
-}
+
