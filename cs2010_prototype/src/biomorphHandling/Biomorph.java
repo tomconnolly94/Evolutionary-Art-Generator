186f7670e8e563fc391b65888b99e3ae500f1976
dmd1337 <dmd1337@hotmail.co.uk>
2015-04-03 23:33:53 +0100

    Implemented gene enumerator in Biomorph
    I HATE JAVA AND HOW UNNECESSARILY COMPLICATED IT MAKES ENUMS
    
    So yeah it turns out Java enums are actually PAINFUL to work with, but
    at least we can now see gene names instead of just their numbers.

--- cs2010_prototype/src/biomorphHandling/Biomorph.java
+++ cs2010_prototype/src/biomorphHandling/Biomorph.java
@@ -9,75 +9,67 @@ import geometry.*;
  */
 public class Biomorph
 {
-	// Enumerator for genes. Intended to replace genes[x] with genes[NAME], but I don't know how enums work in Java.
-	/*
-	 * enum genes
-	 * {
-	 * 		LENGTH,
-	 * 		LENGTH_INCREMENT,
-	 * 		THICKNESS,
-	 * 		THICKNESS_INCREMENT,
-	 * 		BRANCH,
-	 * 		BRANCH_INCREMENT,
-	 * 		CHAIN,
-	 * 		COLOR_RED,
-	 * 		COLOR_GREEN,
-	 * 		COLOR_BLUE,
-	 * 		IRIDESCENCE_RED,
-	 * 		IRIDESCENCE_GREEN,
-	 * 		IRIDESCENCE_BLUE
-	 * }
-	 */
+	// Enumerator for gene names (the "gene-umerator")
+	enum GeneNames
+	{
+		BRANCH,
+		BRANCH_INCREMENT,
+		CHAIN,
+		COLOR_RED,
+		COLOR_GREEN,
+		COLOR_BLUE,
+		LENGTH,
+		LENGTH_INCREMENT,
+		THICKNESS,
+		THICKNESS_INCREMENT,
+		IRIDESCENCE_RED,
+		IRIDESCENCE_GREEN,
+		IRIDESCENCE_BLUE
+	}
+	// After figuring out how to access something as simple as the number a certain value represents, I can now say I HATE Java enums.
+	// Seriously, to access 0, it's genes[GeneNames.LENGTH.ordinal()]. In C++ it would just be genes[LENGTH].
 	// An array is instantiated to hold 13 Genes that will be defined in the constructor.
 	private Gene[] genes = new Gene[13];
 	public Biomorph(int branchVal, int branchIncrementVal, int chainVal, int redVal, int greenVal, int blueVal, int lengthVal, int lengthIncrementVal, int thicknessVal, int thicknessIncrementVal, int iridRedVal, int iridGreenVal, int iridBlueVal)
 	{
 		// Genes are created and assigned values according to the parameters given. They are then placed in an array in
 		// alphabetical (apart from colours which are RGB) order.
-		genes[0] = new Branch(branchVal, "Branch");
-		genes[1] = new BranchIncrement(branchIncrementVal, "Branch Increment");
-		genes[2] = new Chain(chainVal, "Chain");
-		genes[3] = new Color(redVal, "Color Red");
-		genes[4] = new Color(greenVal, "Color Green");
-		genes[5] = new Color(blueVal, "Color Blue");
-		genes[6] = new Length(lengthVal, "Length");
-		genes[7] = new LengthIncrement(lengthIncrementVal, "Length Increment");
-		genes[8] = new Thickness(thicknessVal, "Thickness");
-		genes[9] = new ThicknessIncrement(thicknessIncrementVal, "Thickness Increment");
-		genes[10] = new Iridescence(iridRedVal, "Iridescence Red");
-		genes[11] = new Iridescence(iridGreenVal, "Iridescence Green");
-		genes[12] = new Iridescence(iridBlueVal, "Iridescence Blue");
+		genes[GeneNames.BRANCH.ordinal()] = new Branch(branchVal, "Branch");
+		genes[GeneNames.BRANCH_INCREMENT.ordinal()] = new BranchIncrement(branchIncrementVal, "Branch Increment");
+		genes[GeneNames.CHAIN.ordinal()] = new Chain(chainVal, "Chain");
+		genes[GeneNames.COLOR_RED.ordinal()] = new Color(redVal, "Color Red");
+		genes[GeneNames.COLOR_GREEN.ordinal()] = new Color(greenVal, "Color Green");
+		genes[GeneNames.COLOR_BLUE.ordinal()] = new Color(blueVal, "Color Blue");
+		genes[GeneNames.LENGTH.ordinal()] = new Length(lengthVal, "Length");
+		genes[GeneNames.LENGTH_INCREMENT.ordinal()] = new LengthIncrement(lengthIncrementVal, "Length Increment");
+		genes[GeneNames.THICKNESS.ordinal()] = new Thickness(thicknessVal, "Thickness");
+		genes[GeneNames.THICKNESS_INCREMENT.ordinal()] = new ThicknessIncrement(thicknessIncrementVal, "Thickness Increment");
+		genes[GeneNames.IRIDESCENCE_RED.ordinal()] = new Iridescence(iridRedVal, "Iridescence Red");
+		genes[GeneNames.IRIDESCENCE_GREEN.ordinal()] = new Iridescence(iridGreenVal, "Iridescence Green");
+		genes[GeneNames.IRIDESCENCE_BLUE.ordinal()] = new Iridescence(iridBlueVal, "Iridescence Blue");
 	}
 	/**
 	 * Draws this biomorph.
 	 */
 	public void draw()
 	{
-		// int i = 0;
-		// if (i < genes[8].getValue())
-		// {
 		GL11.glPushMatrix();
 		{
-			loop(genes[2].getValue(), 0);
+			loop(genes[GeneNames.CHAIN.ordinal()].getValue(), 0);
 			GL11.glPushMatrix();
 			{
 				GL11.glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
-				loop(genes[2].getValue(), 0);
+				loop(genes[GeneNames.CHAIN.ordinal()].getValue(), 0);
 				GL11.glPushMatrix();
 				{
 					GL11.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-					loop(genes[2].getValue(), 0);
+					loop(genes[GeneNames.CHAIN.ordinal()].getValue(), 0);
 				}
 				GL11.glPopMatrix();
 			}
 			GL11.glPopMatrix();
 		}
 		GL11.glPopMatrix();
-		// increase the thickness of the biomorph.
-		// i = (genes[8].getValue() + (i+genes[9].getValue()));
-		// i++;
-		// }
-		// i = genes[9].getValue();
 	}
 	/**
 	 * Recursive loop to draw chains from each branch.
@@ -86,11 +78,11 @@ public class Biomorph
 	 */
 	private void loop(int chains, int limbCount)
 	{
-		int length = genes[6].getValue() + ((genes[2].getValue() - chains) * genes[7].getValue());
-		int thickness = genes[8].getValue() + ((genes[2].getValue() - chains) * genes[9].getValue());
-		int red = (genes[3].getValue() + limbCount * genes[10].getValue()) % 256;
-		int green = (genes[4].getValue() + limbCount * genes[11].getValue()) % 256;
-		int blue = (genes[5].getValue() + limbCount * genes[12].getValue()) % 256;
+		int length = genes[GeneNames.LENGTH.ordinal()].getValue() + ((genes[GeneNames.CHAIN.ordinal()].getValue() - chains) * genes[GeneNames.LENGTH_INCREMENT.ordinal()].getValue());
+		int thickness = genes[GeneNames.THICKNESS.ordinal()].getValue() + ((genes[GeneNames.CHAIN.ordinal()].getValue() - chains) * genes[GeneNames.THICKNESS_INCREMENT.ordinal()].getValue());
+		int red = (genes[GeneNames.COLOR_RED.ordinal()].getValue() + limbCount * genes[GeneNames.IRIDESCENCE_RED.ordinal()].getValue()) % 256;
+		int green = (genes[GeneNames.COLOR_GREEN.ordinal()].getValue() + limbCount * genes[GeneNames.IRIDESCENCE_GREEN.ordinal()].getValue()) % 256;
+		int blue = (genes[GeneNames.COLOR_BLUE.ordinal()].getValue() + limbCount * genes[GeneNames.IRIDESCENCE_BLUE.ordinal()].getValue()) % 256;
 		//Prevent length or thickness from going out of bounds
 		if (length < 1) length = 1;
 		if (length > 10) length = 10;
@@ -98,12 +90,12 @@ public class Biomorph
 		if (thickness > 10) thickness = 10;
 		// Set the thickness of the branches (Simple Limb only)
 		GL11.glLineWidth(thickness);
-		for (int b = 0; b < genes[0].getValue(); b++)
+		for (int b = 0; b < genes[GeneNames.BRANCH.ordinal()].getValue(); b++)
 		{
 			GL11.glPushMatrix();
 			{
 				// This allows the distribution of branches to be uniform.
-				GL11.glRotatef((float) b * (360.0f / genes[0].getValue()), 0.0f, 0.0f, 1.0f);
+				GL11.glRotatef((float) b * (360.0f / genes[GeneNames.BRANCH.ordinal()].getValue()), 0.0f, 0.0f, 1.0f);
 				new CuboidLimb(length, thickness, red, green, blue).draw();
 			}
 			GL11.glPopMatrix();
@@ -113,8 +105,8 @@ public class Biomorph
 			{
 				GL11.glPushMatrix();
 				{
-					GL11.glRotatef((float) b * (360.0f / genes[0].getValue()), 0.0f, 0.0f, 1.0f);
-					GL11.glTranslatef(0.0f, genes[6].getValue() + ((genes[2].getValue() - chains + 1) * genes[7].getValue()), 0.0f);
+					GL11.glRotatef((float) b * (360.0f / genes[GeneNames.BRANCH.ordinal()].getValue()), 0.0f, 0.0f, 1.0f);
+					GL11.glTranslatef(0.0f, genes[GeneNames.LENGTH.ordinal()].getValue() + ((genes[GeneNames.CHAIN.ordinal()].getValue() - chains + 1) * genes[GeneNames.LENGTH_INCREMENT.ordinal()].getValue()), 0.0f);
 					loop(chains - 1, limbCount);
 				}
 				GL11.glPopMatrix();
