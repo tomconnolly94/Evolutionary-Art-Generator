01bd3784cd10e38df711a9faf008ac588242cf75
Charandeep <charandeep.rai@gmail.com>
2015-04-20 18:14:06 +0100

    Adding the updated Graphics Main class

--- cs2010_prototype/src/gui/GraphicsMain.java
+++ cs2010_prototype/src/gui/GraphicsMain.java
@@ -15,94 +15,97 @@ import javax.swing.border.EmptyBorder;
  * The main window for the Biomorph Simulation.
  * @author Charandeep Rai
  */
-public class GraphicsMain
+
+public class GraphicsMain{
+
+// The main frame used for the GUI
+private static JFrame mainFrame;
+
+// private FileMenu fileFrame;
+public GraphicsMain()
 {
-	// The main frame used for the GUI
-	private static JFrame mainFrame;
-	// private FileMenu fileFrame;
-	public GraphicsMain()
-	{
-		final JLabel tempRightLabel = new JLabel("Right Panel");
-		final JLabel tempLeftLabel = new JLabel("Biomorph Window");
-		final int blankSpace = 1;
-		// Creating the main container for the GUI
-		mainFrame = new JFrame("Group 5 Biomorph Simulation");
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Importing the filemenu element
-		FileMenu filemenu = new FileMenu();
-		
-		//Importing the Biomorph selection element
-		BiomorphSelector selector = new BiomorphSelector();
-		
-		//Importing the Biomorph display window
-	//	BiomorphWindow biowindow = new BiomorphWindow();
-		
-		
-		// Creating the other containers for the GUI
-		//JButton button = new JButton();
-		
-		JPanel biomorphPanel = new JPanel();
-		JPanel filemenuPanel = new JPanel();
-		JPanel rightPanel = new JPanel();
-		JPanel selectorPanel = new JPanel();
-		
-		// Specifying the layout managers
-		mainFrame.setLayout(new BorderLayout());
-		mainFrame.setPreferredSize(new Dimension(1024,1024));
-		((JPanel) mainFrame.getContentPane()).setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-		
-		biomorphPanel.setLayout(new BorderLayout());
-		biomorphPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-		
-		filemenuPanel.setLayout(new BorderLayout());
-		filemenuPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-		
-		rightPanel.setLayout(new BorderLayout());
-		rightPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-		
-		selectorPanel.setLayout(new BorderLayout());
-		selectorPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-		
-		// Add components to containers
-		filemenuPanel.add(filemenu.getContents(), BorderLayout.CENTER);
-		
-		selectorPanel.add(selector.getContents(), BorderLayout.CENTER);
-		
-		//biomorphPanel.add(biowindow.getContents(), BorderLayout.EAST);
-		
-		rightPanel.add(tempRightLabel, BorderLayout.CENTER);
-		
-		mainFrame.add(filemenuPanel, BorderLayout.NORTH);
-		mainFrame.add(biomorphPanel, BorderLayout.WEST);
-		mainFrame.add(rightPanel, BorderLayout.EAST);
-		mainFrame.add(selectorPanel, BorderLayout.SOUTH);
-		
-				
-		// Action Listeners
-		mainFrame.addWindowListener(new WindowAdapter()
-		{
-			public void Closewindow(WindowEvent e)
-			{
-				exitApp();
-			}
-		});
-		
-		mainFrame.pack();
-		mainFrame.setVisible(true);
-	}
+	final JLabel tempRightLabel = new JLabel("Right Panel");
+	final JLabel tempLeftLabel = new JLabel("Biomorph Window");
+	final int blankSpace = 1;
+	// Creating the main container for the GUI
+	mainFrame = new JFrame("Group 5 Biomorph Simulation");
+	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-	private void exitApp()
+	//Importing the filemenu element
+	FileMenu filemenu = new FileMenu();
+	
+	//Importing the Biomorph selection element
+	BiomorphSelector selector = new BiomorphSelector();
+	
+	//Importing the Biomorph display window
+	BiomorphWindows biowindow = new BiomorphWindows();
+	
+	
+	// Creating the other containers for the GUI
+	//JButton button = new JButton();
+	
+	JPanel biomorphPanel = new JPanel();
+	JPanel filemenuPanel = new JPanel();
+	JPanel rightPanel = new JPanel();
+	JPanel selectorPanel = new JPanel();
+	
+	// Specifying the layout managers
+	mainFrame.setLayout(new BorderLayout());
+	mainFrame.setPreferredSize(new Dimension(800,800));
+	((JPanel) mainFrame.getContentPane()).setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+	
+	biomorphPanel.setLayout(new BorderLayout());
+	biomorphPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+	
+	filemenuPanel.setLayout(new BorderLayout());
+	filemenuPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+	
+	rightPanel.setLayout(new BorderLayout());
+	rightPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+	
+	selectorPanel.setLayout(new BorderLayout());
+	selectorPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+	
+	// Add components to containers
+	filemenuPanel.add(filemenu.getContents(), BorderLayout.CENTER);
+	
+	selectorPanel.add(selector.getContents(), BorderLayout.CENTER);
+	
+	biomorphPanel.add(biowindow.getContents(), BorderLayout.EAST);
+	
+	rightPanel.add(tempRightLabel, BorderLayout.CENTER);
+	
+	mainFrame.add(filemenuPanel, BorderLayout.NORTH);
+	mainFrame.add(biomorphPanel, BorderLayout.WEST);
+	mainFrame.add(rightPanel, BorderLayout.EAST);
+	mainFrame.add(selectorPanel, BorderLayout.SOUTH);
+	
+			
+	// Action Listeners
+	mainFrame.addWindowListener(new WindowAdapter()
 	{
-		int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-		if (response == JOptionPane.YES_OPTION)
+		public void Closewindow(WindowEvent e)
 		{
-			System.exit(0);
+			exitApp();
 		}
-	}
+	});
 	
-	public static void main(String[] args)
+	mainFrame.pack();
+	mainFrame.setVisible(true);
+}
+
+private void exitApp()
+{
+	int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+	if (response == JOptionPane.YES_OPTION)
 	{
-		GraphicsMain gm = new GraphicsMain();
+		System.exit(0);
 	}
+}
+
+public static void main(String[] args)
+{
+	GraphicsMain gm = new GraphicsMain();
+}
+
 }
