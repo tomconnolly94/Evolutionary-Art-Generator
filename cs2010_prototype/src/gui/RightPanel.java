8fea96759da041fc2f0b05c88e5347b88f44d5d8
Tom Connolly <tom.connolly2511@gmail.com>
2015-04-20 11:12:49 +0100

    bens commit of rightpanel.

--- cs2010_prototype/src/gui/RightPanel.java
+++ cs2010_prototype/src/gui/RightPanel.java
@@ -1,17 +1,19 @@
 package gui;
 import java.awt.*;
 import javax.swing.*;
-/**
- * The right panel of the GUI
- * @author Charandeep Rai
- */
 public class RightPanel
 {
+	/**
+	 * The right panel of the GUI
+	 * @author Charandeep Rai
+	 */
 	// Frames used
-	private static JFrame opening;
-	private static JFrame createClicked;
-	private static JFrame modifyPane;
-	private static JFrame colour;
+	private static JFrame viewFrame;
+	// Panels used
+	private static JPanel openingPane;
+	private static JPanel createClicked;
+	private static JPanel modifyPane;
+	private static JPanel colourPane;
 	// Buttons used
 	private static JButton create;
 	private static JButton modify;
@@ -20,7 +22,6 @@ public class RightPanel
 	private static JButton green;
 	private static JButton red;
 	private static JButton blue;
-	private static JButton back;
 	// Sliders used
 	private static JSlider chain;
 	private static JSlider branch;
@@ -36,9 +37,6 @@ public class RightPanel
 	private static JLabel curvatureLabel;
 	private static JLabel lengthIncLabel;
 	private static JLabel thicknessIncLabel;
-	private static JLabel redlabel;
-	private static JLabel greenlabel;
-	private static JLabel bluelabel;
 	public RightPanel()
 	{
 		initiate();
@@ -46,63 +44,105 @@ public class RightPanel
 	public void initiate()
 	{
 		// Create instance of side container
-		opening = new JFrame("Menu");
-		opening.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		viewFrame = new JFrame("Menu");
+		openingPane = new JPanel();
+		viewFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
 		// Listeners
 		// setTitle
-		opening.setTitle("Menu");
+		viewFrame.setTitle("Menu");
 		// Create Buttons
-		create = new JButton("Create");
-		random = new JButton("Random");
+		create = new JButton();
+		create.setText("Create");
+		random = new JButton();
+		random.setText("Random");
 		// Set Layout
-		opening.setLayout(new BoxLayout(opening, BoxLayout.Y_AXIS));
-		opening.add(Box.createRigidArea(new Dimension(0, 5)));
-	}
-	public void createClicked()
-	{
+		openingPane.setLayout(new BoxLayout(openingPane, BoxLayout.Y_AXIS));
+		openingPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		// add components to panel
+		openingPane.add(create);
+		openingPane.add(random);
+		// add panel to frame
+		viewFrame.add(openingPane);
+		// make visible
+		viewFrame.setVisible(true);
 		// Create instance of side container
-		createClicked = new JFrame("Menu");
-		createClicked.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-		createClicked.setTitle("Menu");
-		modify = new JButton("Modify");
-		random = new JButton("Random");
-		evolve = new JButton("Evolve");
+		modify = new JButton();
+		modify.setText("Modify");
+		random = new JButton();
+		random.setText("Random");
+		evolve = new JButton();
+		evolve.setText("Evolve");
 		// Set Layout
 		createClicked.setLayout(new BoxLayout(createClicked, BoxLayout.Y_AXIS));
-		createClicked.add(Box.createRigidArea(new Dimension(0, 5)));
-	}
-	public void modify()
-	{
-		modifyPane = new JFrame("Menu");
-		modifyPane.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-		modifyPane.setTitle("Menu");
+		createClicked.setBorder(BorderFactory.createLineBorder(Color.black));
+		// add to separate Panel
+		createClicked.add(modify);
+		createClicked.add(random);
+		createClicked.add(evolve);
 		// initialise the sliders, I need to find out the ranges of the genes.
 		chainLabel = new JLabel("Chain");
 		chain = new JSlider();
 		chainLabel.setLabelFor(chain);
 		branchLabel = new JLabel("Branch");
 		branch = new JSlider();
+		branchLabel.setLabelFor(branch);
 		lengthLabel = new JLabel("Length");
 		length = new JSlider();
+		lengthLabel.setLabelFor(length);
 		thicknessLabel = new JLabel("Thickness");
 		thickness = new JSlider();
+		thicknessLabel.setLabelFor(thickness);
 		curvatureLabel = new JLabel("Curvature");
 		curvature = new JSlider();
+		curvatureLabel.setLabelFor(curvature);
 		lengthIncLabel = new JLabel("Length Increment");
 		lengthIncrement = new JSlider();
+		lengthIncLabel.setLabelFor(lengthIncrement);
 		thicknessIncLabel = new JLabel("Thickness Increment");
 		thicknessIncrement = new JSlider();
-		modify.setLayout(new BoxLayout(opening, BoxLayout.Y_AXIS));
-		modify.add(Box.createRigidArea(new Dimension(0, 5)));
-		colour = new JFrame("Colour");
-		red = new JButton("Red");
-		green = new JButton("Green");
-		blue = new JButton("Blue");
-		colour.setLayout(new BoxLayout(opening, BoxLayout.X_AXIS));
-		colour.add(Box.createRigidArea(new Dimension(0, 5)));
+		thicknessIncLabel.setLabelFor(thicknessIncrement);
+		modifyPane.setLayout(new BoxLayout(modifyPane, BoxLayout.Y_AXIS));
+		modifyPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		modifyPane.add(chainLabel);
+		modifyPane.add(chain);
+		modifyPane.add(branchLabel);
+		modifyPane.add(branch);
+		modifyPane.add(lengthLabel);
+		modifyPane.add(length);
+		modifyPane.add(thicknessLabel);
+		modifyPane.add(thickness);
+		modifyPane.add(curvatureLabel);
+		modifyPane.add(curvature);
+		modifyPane.add(lengthIncLabel);
+		modifyPane.add(lengthIncrement);
+		modifyPane.add(thicknessIncLabel);
+		modifyPane.add(thicknessIncrement);
+		red = new JButton();
+		red.setText("Red");
+		green = new JButton();
+		green.setText("Green");
+		blue = new JButton();
+		blue.setText("Blue");
+		colourPane.setLayout(new BoxLayout(colourPane, BoxLayout.X_AXIS));
+		colourPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		colourPane.add(red);
+		colourPane.add(green);
+		colourPane.add(blue);
 	}
-	public static void main(String[] args)
+	public void createClicked()
 	{
-		RightPanel rp = new RightPanel();
+		/**
+		 * changes view from first menu to second. Still not sure if this is the
+		 * most efficient way to do this or not, could do with some help as this
+		 * was where I was going to use the action listeners
+		 */
+		viewFrame.remove(openingPane);
+		viewFrame.add(createClicked);
+	}
+	public void modify()
+	{
+		viewFrame.remove(createClicked);
+		viewFrame.add(modify);
+		viewFrame.add(colourPane);
 	}
 }
