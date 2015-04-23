15fe80ab821424958092fa44c944d96612bd483e
Tom Connolly <tom.connolly2511@gmail.com>
2015-04-23 19:35:34 +0100

    redone some stuff

--- cs2010_prototype/src/gui/BiomorphCustomisation.java
+++ cs2010_prototype/src/gui/BiomorphCustomisation.java
@@ -38,81 +38,77 @@ public class BiomorphCustomisation {
 	private static JLabel blueLabel;
 	
 	public BiomorphCustomisation(){
-		initiate();
+		viewFrame = new JFrame();	
+		modifyPane = new JPanel();
+		colourPane = new JPanel();
+		chainLabel = new JLabel("Chain");
+		chain = new JSlider();
+		chainLabel.setLabelFor(chain);
+		branchLabel = new JLabel("Branch");
+		branch = new JSlider();
+		branchLabel.setLabelFor(branch);
+		lengthLabel = new JLabel("Length");
+		length = new JSlider();
+		lengthLabel.setLabelFor(length);
+		thicknessLabel = new JLabel("Thickness");
+		thickness = new JSlider();
+		thicknessLabel.setLabelFor(thickness);
+		curvatureLabel = new JLabel("Curvature");
+		curvature = new JSlider();
+		curvatureLabel.setLabelFor(curvature);
+		lengthIncLabel = new JLabel("Length Increment");
+		lengthIncrement = new JSlider();
+		lengthIncLabel.setLabelFor(lengthIncrement);
+		thicknessIncLabel = new JLabel("Thickness Increment");
+		thicknessIncrement = new JSlider();
+		thicknessIncLabel.setLabelFor(thicknessIncrement);
+
+		modifyPane.setLayout(new BoxLayout(modifyPane, BoxLayout.Y_AXIS));
+		modifyPane.setBorder(BorderFactory.createLineBorder(Color.black));
+
+		modifyPane.add(chainLabel);
+		modifyPane.add(chain);
+		modifyPane.add(branchLabel);
+		modifyPane.add(branch);
+		modifyPane.add(lengthLabel);
+		modifyPane.add(length);
+		modifyPane.add(thicknessLabel);
+		modifyPane.add(thickness);
+		modifyPane.add(curvatureLabel);
+		modifyPane.add(curvature);
+		modifyPane.add(lengthIncLabel);
+		modifyPane.add(lengthIncrement);
+		modifyPane.add(thicknessIncLabel);
+		modifyPane.add(thicknessIncrement);
+
+		red = new JSlider();
+		redLabel = new JLabel("Red");
+		redLabel.setLabelFor(red);
+		
+		green = new JSlider();
+		greenLabel = new JLabel("Green");
+		greenLabel.setLabelFor(green);
+		
+		blue = new JSlider();
+		blueLabel = new JLabel("Blue");
+		blueLabel.setLabelFor(blue);
+
+
+		colourPane.setLayout(new BoxLayout(colourPane, BoxLayout.X_AXIS));
+		colourPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		colourPane.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace) );
+		
+		colourPane.add(red);
+		colourPane.add(redLabel);
+		colourPane.add(green);
+		colourPane.add(blueLabel);
+		colourPane.add(blue);
+		colourPane.add(greenLabel);
+		
+		viewFrame.add(colourPane, BorderLayout.SOUTH);
+		viewFrame.add(modifyPane, BorderLayout.NORTH);
 	}
-	
-	public void initiate(){
-	viewFrame = new JFrame();	
-	modifyPane = new JPanel();
-	colourPane = new JPanel();
-	chainLabel = new JLabel("Chain");
-	chain = new JSlider();
-	chainLabel.setLabelFor(chain);
-	branchLabel = new JLabel("Branch");
-	branch = new JSlider();
-	branchLabel.setLabelFor(branch);
-	lengthLabel = new JLabel("Length");
-	length = new JSlider();
-	lengthLabel.setLabelFor(length);
-	thicknessLabel = new JLabel("Thickness");
-	thickness = new JSlider();
-	thicknessLabel.setLabelFor(thickness);
-	curvatureLabel = new JLabel("Curvature");
-	curvature = new JSlider();
-	curvatureLabel.setLabelFor(curvature);
-	lengthIncLabel = new JLabel("Length Increment");
-	lengthIncrement = new JSlider();
-	lengthIncLabel.setLabelFor(lengthIncrement);
-	thicknessIncLabel = new JLabel("Thickness Increment");
-	thicknessIncrement = new JSlider();
-	thicknessIncLabel.setLabelFor(thicknessIncrement);
 
-	modifyPane.setLayout(new BoxLayout(modifyPane, BoxLayout.Y_AXIS));
-	modifyPane.setBorder(BorderFactory.createLineBorder(Color.black));
-
-	modifyPane.add(chainLabel);
-	modifyPane.add(chain);
-	modifyPane.add(branchLabel);
-	modifyPane.add(branch);
-	modifyPane.add(lengthLabel);
-	modifyPane.add(length);
-	modifyPane.add(thicknessLabel);
-	modifyPane.add(thickness);
-	modifyPane.add(curvatureLabel);
-	modifyPane.add(curvature);
-	modifyPane.add(lengthIncLabel);
-	modifyPane.add(lengthIncrement);
-	modifyPane.add(thicknessIncLabel);
-	modifyPane.add(thicknessIncrement);
-
-	red = new JSlider();
-	redLabel = new JLabel("Red");
-	redLabel.setLabelFor(red);
-	
-	green = new JSlider();
-	greenLabel = new JLabel("Green");
-	greenLabel.setLabelFor(green);
-	
-	blue = new JSlider();
-	blueLabel = new JLabel("Blue");
-	blueLabel.setLabelFor(blue);
-
-
-	colourPane.setLayout(new BoxLayout(colourPane, BoxLayout.X_AXIS));
-	colourPane.setBorder(BorderFactory.createLineBorder(Color.black));
-	colourPane.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace) );
-	
-	colourPane.add(red);
-	colourPane.add(redLabel);
-	colourPane.add(green);
-	colourPane.add(blueLabel);
-	colourPane.add(blue);
-	colourPane.add(greenLabel);
-	
-	viewFrame.add(colourPane, BorderLayout.SOUTH);
-	viewFrame.add(modifyPane, BorderLayout.NORTH);
-	
-}
 	public JPanel getContents()
 	{
 		return (JPanel)viewFrame.getContentPane();
--- cs2010_prototype/src/gui/BiomorphWindows.java
+++ cs2010_prototype/src/gui/BiomorphWindows.java
@@ -1,30 +1,29 @@
 package gui;
+import java.awt.Frame;
+import java.util.List;
+import javax.media.nativewindow.NativeSurface;
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLAnimatorControl;
 import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilitiesImmutable;
+import javax.media.opengl.GLDrawable;
+import javax.media.opengl.GLDrawableFactory;
 import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLException;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLRunnable;
 import javax.media.opengl.awt.GLCanvas;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
 import org.lwjgl.util.glu.GLU;
-import com.jogamp.opengl.util.Animator;
-import com.jogamp.opengl.util.FPSAnimator;
+import com.jogamp.common.util.locks.RecursiveLock;
 import biomorphHandling.BiomorphManager;
-import biomorphHandling.Drawer;
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import org.lwjgl.input.Keyboard;
 
 /**
  * The Biomorph window using JOGL and Swing Components
@@ -33,7 +32,7 @@ import org.lwjgl.input.Keyboard;
  *
  */
 
-public class BiomorphWindows implements GLEventListener {
+public class BiomorphWindows{
 
 	private static boolean quit;
 	private boolean keystop;
@@ -43,211 +42,19 @@ public class BiomorphWindows implements GLEventListener {
 	private float zoom;
 	private BiomorphManager bm;
 	private JFrame frame;
+	
 	public BiomorphWindows(){
-		quit = false;
-		keystop = false;
-		aspect = 0.0f;
-		lat = 0.0f;
-		lon = 0.0f;
-		zoom = 1.0f;
-		bm = new BiomorphManager();
+		//setup JOGL properties
+		GLProfile glpr = GLProfile.getDefault();
+		GLProfile.initSingleton();
+		GLCapabilities glca = new GLCapabilities(glpr);
+		GLCanvas canvas = new GLCanvas();
+		Frame frame = new Frame();
 		
-		GLProfile profile = GLProfile.getDefault();
-        GLCapabilities capabilities = new GLCapabilities(profile);
-        GLCanvas canvas = new GLCanvas(capabilities);
-
-        frame = new JFrame("Test Biomorph Window");
-        frame.setSize(800, 600);
-        frame.add(canvas);
-        frame.setVisible(true);
-		
-		// Set display to 1024x768, windowed
-				DisplayMode mode = new DisplayMode(200, 150);
-				try
-				{
-					Display.setDisplayMode(mode);
-					Display.setParent(canvas);
-					Display.create();
-					
-				}
-				catch (Exception e)
-				{
-				}
-				// Set the display's aspect ratio (in this case, 4:3)
-				aspect = (float) mode.getWidth() / (float) mode.getHeight();
-		
-
-        
-        
-
-        
-
-        //canvas.addGLEventListener(new BiomorphWindow());
-        
-        while(quit==false){
-        	reshape();
-        }
-        
+		frame.add(canvas);
 	}
 
     public static void main(String[] args) {
      BiomorphWindows bw = new BiomorphWindows(); 
     }
-
-    @Override
-    public void display(GLAutoDrawable drawable) {
-        //render(drawable);
-    	//Drawer d = new Drawer();
-    	
-//    	while (dquit == false) {
-//    		d.checkInput();
-//    		d.draw();
-//    	}
-//    	frame.destroy();
-//    }
-    }
-    @Override
-    public void dispose(GLAutoDrawable drawable) {
-    }
-
-    @Override
-    public void init(GLAutoDrawable drawable) {
-    }
-
-    public void reshape(/*GLAutoDrawable drawable, int x, int y, int w, int h*/) {
-    	// Pressing Enter will generate a new biomorph.
-    			/*if (Keyboard.isKeyDown(Keyboard.KEY_RETURN))
-    			{
-    				// evolves two biomorphs together
-    				if (keystop == false)
-    				{
-    					bm.addSpecific(bm.evolveClo(bm.getSpecific(0), bm.getRandomBiomorph()));
-    					bm.createAndAdd();
-    					keystop = true;
-    				}
-    			}
-    			// This prevents repeated creations of biomorphs while the Enter key
-    			// is held down.
-    			if (Keyboard.isKeyDown(Keyboard.KEY_RETURN) == false) keystop = false;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) quit = true;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_UP)) if (lat < 90.0f) lat += 2.0f;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) if (lat > -90.0f) lat -= 2.0f;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) lon -= 2.0f;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) lon += 2.0f;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_W)) if (zoom > 0) zoom -= 0.01f;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_S)) zoom += 0.01f;
-    			if (Display.isCloseRequested()) quit = true;*/
-    			
-    			// Clear screen and draw biomorph
-    			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-    			GL11.glPushMatrix();
-    			{
-    				// Positions the camera according to latitude and longitude, as if
-    				// the biomorph is a globe-like object
-    				GL11.glOrtho(-80.0f * aspect * zoom, 80.0f * aspect * zoom, -80.0f * zoom, 80.0f * zoom, -80.0f * zoom, 80.0f * zoom);
-    				GLU.gluLookAt((float) Math.cos(Math.toRadians(lat)) * -(float) Math.cos(Math.toRadians(lon)), (float) Math.sin(Math.toRadians(lat)), (float) Math.cos(Math.toRadians(lat)) * (float) Math.sin(Math.toRadians(lon)), 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-    				bm.getSpecific(0).draw();
-    			}
-    			GL11.glPopMatrix();
-    			Display.update();
-    			// Limit to 60fps to save CPU usage
-    			Display.sync(60);	
-
-    }
-
-
-    private void render(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
-
-        gl.glBegin(GL.GL_TRIANGLES);
-        gl.glColor3f(1, 0, 0);
-        gl.glVertex2d(1, 1);
-        gl.glVertex2d(0, 1);
-        gl.glVertex2d(1, -1);
-        gl.glEnd();
-    }
-
-	@Override
-	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-	public JPanel getContents(){
-		 return (JPanel)frame.getContentPane();
-		 
-		   	}
 }
-
-
-
-
-//public class BiomorphWindow {
-//	
-//	 final JFrame window = new JFrame(); 
-//	
-//	public static void main( String [] args) {
-//		//Sets it to a default GL Profile
-//		GLProfile profile = GLProfile.getDefault();
-//		//Sets the OpenGL capabilities 
-//		GLCapabilities capabilities = new GLCapabilities(profile);
-//		//Applies those capabilities to a new OpenGL Canvas
-//		GLCanvas canvas = new GLCanvas( capabilities);
-//
-////		canvas.addGLEventListener( new GLEventListener() {
-////			
-////			//TODO: Complete the init method as well as the display method, once this is complete the Biomorph will display in the GUI
-////			
-////			
-////		       @Override
-////	            public void init( GLAutoDrawable glautodrawable ) {
-////	            }
-////	            
-////	            @Override
-////	            public void dispose( GLAutoDrawable glautodrawable ) {
-////	            }
-////	            
-//////	            @Override
-//////	            public void display( GLAutoDrawable glautodrawable ) {
-////////	               DisplayMode mode = new DisplayMode(1024, 768);
-////////	            	
-////////	            	try
-////////					{
-////////						Display.setDisplayMode(mode);
-////////						Display.create();
-////////						Display.setTitle("Biomorph Display");
-////////					}
-////////					catch (Exception e)
-////////					{
-////////						e.printStackTrace();
-////////					}
-////////	                
-//////	            }
-//////
-//////				@Override
-//////				public void reshape(GLAutoDrawable glautodrawable, int aspect, int lat, int lon, int zoom)
-//////				{
-////////					Drawer.draw(glautodrawable.getGL(), aspect, lat, lon, zoom);
-//////				}
-////
-////			
-////
-////		}
-////				);
-////
-////	}
-//	
-//
-//	protected void initialise()
-//	{
-//		window.add(canvas);
-//		window.setSize(640,480);
-//		window.pack();
-//		window.setVisible(true);
-//	}
-////	
-////	
-//	 public JPanel getContents(){
-//	 return (JPanel)window.getContentPane();//.add( canvas, BorderLayout.CENTER);
-//	   	}
-//}
