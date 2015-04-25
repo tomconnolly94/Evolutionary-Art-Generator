2e674247ccab5a32d9260fe93b3cf7f83226126b
dmd1337 <dmd1337@hotmail.co.uk>
2015-04-25 21:34:41 +0100

    Commented out redundant classes
    Just to remove the red crosses

--- cs2010_prototype/src/gui/GraphicsMain.java
+++ cs2010_prototype/src/gui/GraphicsMain.java
@@ -27,121 +27,102 @@ import com.jogamp.opengl.util.FPSAnimator;
  * The main window for the Biomorph Simulation.
  * @author Charandeep Rai
  */
-
-public class GraphicsMain{
-
-// The main frame used for the GUI
-private static JFrame mainFrame;
-
-// private FileMenu fileFrame;
-public GraphicsMain()
+public class GraphicsMain
 {
-	final JLabel tempRightLabel = new JLabel("Right Panel");
-	final JLabel tempLeftLabel = new JLabel("Biomorph Window");
-	final int blankSpace = 1;
-	// Creating the main container for the GUI
-	mainFrame = new JFrame("Group 5 Biomorph Simulation");
-	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	//Importing the filemenu element
-	FileMenu filemenu = new FileMenu();
-	
-	//Importing the Biomorph selection element
-	//BiomorphSelector selector = new BiomorphSelector();
-	
-	//Importing the Biomorph display window
-	//BiomorphWindows biowindow = new BiomorphWindows();
-	
-	//Importing the RightPanel
-	RightPanel rp = new RightPanel();
-	
-	//setting up the biomorph window
-	GLProfile glp = GLProfile.getDefault();
-	GLCapabilities caps = new GLCapabilities(glp);
-	GLCanvas canvas = new GLCanvas(caps);
-	JPanel bioWindow = new JPanel();
-	int width = 800;
-	int height = 800;
-	canvas.setSize(new Dimension(width-150,height-150));
-	bioWindow.add(canvas);
-	bioWindow.setSize(new Dimension(width, height));
-	bioWindow.setVisible(true);
-	OpenGLFrame oframe = new OpenGLFrame();
-	canvas.addGLEventListener(oframe);
-	canvas.addKeyListener(oframe);
-	FPSAnimator animator = new FPSAnimator(canvas, 60);
-	animator.start();
-	
-	
-	
-	// Creating the other containers for the GUI	
-	JPanel filemenuPanel = new JPanel();
-	JPanel rightPanel = new JPanel();
-	JPanel bigBiomorphDisplayArea = new JPanel(new GridBagLayout());
-	
-	
-	
-	// Specifying the layout managers
-	mainFrame.setLayout(new BorderLayout());
-	mainFrame.setPreferredSize(new Dimension(800,800));
-	((JPanel) mainFrame.getContentPane()).setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-
-	filemenuPanel.setLayout(new BorderLayout());
-	filemenuPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	
-	rightPanel.setLayout(new BorderLayout());
-	rightPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	
-	GridBagConstraints gbc = new GridBagConstraints();
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.gridx=0;
-	gbc.gridy=0;
-	//bigBiomorphDisplayArea.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
-	bigBiomorphDisplayArea.setMinimumSize(new Dimension(width,height));
-	
-	
-	
-	// Add components to containers
-	filemenuPanel.add(filemenu.getContents(), BorderLayout.CENTER);
-	
-	rightPanel.add(rp.getContents(), BorderLayout.CENTER);
-	
-	bigBiomorphDisplayArea.add(canvas, gbc);
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.gridx=1;
-	gbc.gridy=0;
-	bigBiomorphDisplayArea.add(rightPanel, gbc);
-	
-	mainFrame.add(filemenuPanel, BorderLayout.NORTH);
-	mainFrame.add(bigBiomorphDisplayArea, BorderLayout.CENTER);
-	
-	
-	
-	// Action Listeners
-	mainFrame.addWindowListener(new WindowAdapter()
+	// The main frame used for the GUI
+	private static JFrame mainFrame;
+	// private FileMenu fileFrame;
+	public GraphicsMain()
 	{
-		public void Closewindow(WindowEvent e)
+		final JLabel tempLeftLabel = new JLabel("Biomorph Window");
+		final JLabel tempRightLabel = new JLabel("Right Panel");
+		final int blankSpace = 1;
+		
+		// Creating the main container for the GUI
+		mainFrame = new JFrame("Group 5 Biomorph Simulation");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Importing the filemenu element
+		FileMenu filemenu = new FileMenu();
+		
+		// Importing the Biomorph selection element
+		// BiomorphSelector selector = new BiomorphSelector();
+		
+		// Importing the Biomorph display window
+		// BiomorphWindows biowindow = new BiomorphWindows();
+		
+		// Setting up the biomorph window
+		GLCanvas canvas = new GLCanvas(new GLCapabilities(GLProfile.getDefault()));
+		JPanel bioWindow = new JPanel();
+		int width = 800;
+		int height = 800;
+		canvas.setSize(new Dimension(width - 110, height - 110));
+		bioWindow.add(canvas);
+		bioWindow.setSize(new Dimension(width, height));
+		bioWindow.setVisible(true);
+		OpenGLFrame oframe = new OpenGLFrame();
+		canvas.addGLEventListener(oframe);
+		canvas.addKeyListener(oframe);
+		FPSAnimator animator = new FPSAnimator(canvas, 120);
+		animator.start();
+		
+		// Importing the RightPanel
+		RightPanel rp = new RightPanel();
+		
+		// Creating the other containers for the GUI
+		JPanel filemenuPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
+		JPanel bigBiomorphDisplayArea = new JPanel(new GridBagLayout());
+		
+		// Specifying the layout managers
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.setPreferredSize(new Dimension(800, 800));
+		((JPanel) mainFrame.getContentPane()).setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+		
+		filemenuPanel.setLayout(new BorderLayout());
+		filemenuPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+		
+		rightPanel.setLayout(new BorderLayout());
+		rightPanel.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		// bigBiomorphDisplayArea.setBorder(new EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
+		bigBiomorphDisplayArea.setMinimumSize(new Dimension(width, height));
+		
+		// Add components to containers
+		filemenuPanel.add(filemenu.getContents(), BorderLayout.CENTER);
+		rightPanel.add(rp.getContents(), BorderLayout.CENTER);
+		bigBiomorphDisplayArea.add(canvas, gbc);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		bigBiomorphDisplayArea.add(rightPanel, gbc);
+		mainFrame.add(filemenuPanel, BorderLayout.NORTH);
+		mainFrame.add(bigBiomorphDisplayArea, BorderLayout.CENTER);
+		// Action Listeners
+		mainFrame.addWindowListener(new WindowAdapter()
 		{
-			exitApp();
-		}
-	});
-	
-	mainFrame.pack();
-	mainFrame.setVisible(true);
-}
-
-private void exitApp()
-{
-	int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-	if (response == JOptionPane.YES_OPTION)
-	{
-		System.exit(0);
+			public void Closewindow(WindowEvent e)
+			{
+				exitApp();
+			}
+		});
+		mainFrame.pack();
+		mainFrame.setVisible(true);
 	}
-}
-
-public static void main(String[] args)
-{
-	GraphicsMain gm = new GraphicsMain();
-}
-
+	private void exitApp()
+	{
+		int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		if (response == JOptionPane.YES_OPTION)
+		{
+			System.exit(0);
+		}
+	}
+	public static void main(String[] args)
+	{
+		GraphicsMain gm = new GraphicsMain();
+	}
 }
