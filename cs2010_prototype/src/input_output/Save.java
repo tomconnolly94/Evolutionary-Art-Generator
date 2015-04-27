adbc1103842c36de7213c765230666c0564c39fd
Tom Connolly <tom.connolly2511@gmail.com>
2015-04-27 20:22:17 +0100

    editing save to try to build save to png feature. progress slow.

--- cs2010_prototype/src/input_output/Save.java
+++ cs2010_prototype/src/input_output/Save.java
@@ -1,9 +1,13 @@
 package input_output;
 import genes.Gene;
 import gui.OpenGLFrame;
+import java.awt.AWTException;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Robot;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -22,6 +26,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import jogamp.opengl.glu.mipmap.Image;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 import biomorphHandling.*;
 import com.jogamp.opengl.util.FPSAnimator;
 /**
@@ -33,19 +38,59 @@ public class Save
 {
 	static FileOutputStream fop = null;
 	static File file;
+	private String fileDest = "C:/Users/Tom/Pictures/biomorphImages/biomorphImage.png";
 
-	public Save(GLCanvas canvas)
+	public Save(GLCanvas canvas, GL2 gl) throws AWTException
 	{
-		BufferedImage image = new BufferedImage(canvas.getWidth(), canvas.getHeight(), BufferedImage.TYPE_INT_RGB);
-		System.out.println("YES");
+		BufferedImage screenshot = new BufferedImage(canvas.getWidth(), canvas.getHeight(), BufferedImage.TYPE_INT_RGB);
+	    Graphics graphics = screenshot.getGraphics();
+
+	/*    ByteBuffer buffer = BufferUtils.createByteBuffer(canvas.getWidth() * canvas.getHeight() * 3);
+
+	    //gl.glReadPixels(0, 0, canvas.getWidth(), canvas.getHeight(), gl.GL_RGB, gl.GL_BYTE, buffer);
+
+
+	    for (int h = 0; h < canvas.getHeight(); h++) {
+	        for (int w = 0; w < canvas.getWidth(); w++) {
+	            // The color are the three consecutive bytes, it's like referencing
+	            // to the next consecutive array elements, so we got red, green, blue..
+	            // red, green, blue, and so on..
+	            graphics.setColor(new Color( buffer.get()*2, buffer.get()*2, buffer.get()*2 ));
+	            graphics.drawRect(w,canvas.getHeight() - h, 1, 1); // canvas.getHeight() - h is for flipping the image
+	        }
+	    }*/
+	    try {
+            ImageIO.write(screenshot, "PNG", new File(fileDest));
+        } catch (IOException ex) {
+        }
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws AWTException{
+		GLCanvas canvas = new GLCanvas(new GLCapabilities(GLProfile.getDefault()));
+		BiomorphCreator bc = new BiomorphCreator();
+		OpenGLFrame oframe = new OpenGLFrame(bc.createBiomorph());
+		canvas.addGLEventListener(oframe);
+		canvas.addKeyListener(oframe);
+		int width = 400;
+		int height = 400;
+		JFrame frame = new JFrame("FRAME");
+		JPanel panel = new JPanel();
+		panel.setSize(width, height);
+		frame.setSize(width, height);
+		frame.add(canvas);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		frame.add(panel);
+		FPSAnimator animator = new FPSAnimator(canvas, 60);
+		animator.start();
 		
-		
-		
-		
-		Save save = new Save();
+		Save save = new Save(canvas, oframe.getGL2());
 	}
 	
 	
