71def921115d9beb85e61b76144d26310087c56e
dmd1337 <dmd1337@hotmail.co.uk>
2015-04-29 14:03:08 +0100

    Actually fixed window listener

--- cs2010_prototype/src/gui/GraphicsMain.java
+++ cs2010_prototype/src/gui/GraphicsMain.java
@@ -17,31 +17,40 @@ import biomorphHandling.*;
 /**
  * The main window for the Biomorph Simulation.
  * @author Charandeep Rai, Jack Taylor, Tom Connolly
- * @version 28/04/2015
+ * @version 29/04/2015
  */
 public class GraphicsMain
 {
-	private static JFrame mainFrame; // The main frame used for the GUI
-	private static BiomorphManager bm; // The Biomorph Manager used to arrange and organise Biomorphs
+	private JFrame mainFrame; // The main frame used for the GUI
+	private FileMenu fileMenu;
+	private JPanel contentPanel;
+	private JPanel biomorphPanel;
+	private JPanel mutatePanel;
+	private JPanel largeBiomorphWindow;
+	private JPanel smallBiomorphWindow[];
+	private RightPanel rp;
+	private BiomorphManager bm; // The Biomorph Manager used to arrange and organise Biomorphs
+	private int largeBiomorphWindowSize;
+	private int smallBiomorphWindowSize;
 	public GraphicsMain()
 	{
 		// *0* Initialise variables
 		final int blankSpace = 1;
 		int width = 800;
 		int height = 800;
-		int largeBiomorphWindowSize = 600;
-		int smallBiomorphWindowSize = 150;
+		largeBiomorphWindowSize = 400;
+		smallBiomorphWindowSize = 100;
 		bm = new BiomorphManager();
 		// *1* Create components
 		mainFrame = new JFrame("Group 5 Biomorph Simulation");
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		FileMenu fileMenu = new FileMenu();
-		JPanel largeBiomorphWindow = new JPanel();
-		JPanel smallBiomorphWindow[] = new JPanel[8];
+		mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		fileMenu = new FileMenu();
+		largeBiomorphWindow = new JPanel();
+		smallBiomorphWindow = new JPanel[8];
 		for (int i = 0; i < smallBiomorphWindow.length; i++) smallBiomorphWindow[i] = new JPanel();
-		RightPanel rp = new RightPanel();
+		rp = new RightPanel();
 		OpenGLFrame oframe[] = new OpenGLFrame[9];
-		// *2* Set up the biomorph window
+		// *2* Set up the biomorph windows
 		oframe[0] = new OpenGLFrame(bm.getRandomBiomorph(), largeBiomorphWindowSize);
 		largeBiomorphWindow.add(oframe[0].getCanvas());
 		largeBiomorphWindow.setSize(largeBiomorphWindowSize, largeBiomorphWindowSize);
@@ -55,9 +64,9 @@ public class GraphicsMain
 			smallBiomorphWindow[i - 1].setVisible(true);
 		}
 		// *3* Create containers
-		JPanel contentPanel = new JPanel(new GridBagLayout());
-		JPanel biomorphPanel = new JPanel();
-		JPanel mutatePanel = new JPanel(new GridBagLayout());
+		contentPanel = new JPanel(new GridBagLayout());
+		biomorphPanel = new JPanel();
+		mutatePanel = new JPanel(new GridBagLayout());
 		mutatePanel.setSize(smallBiomorphWindowSize * 4, smallBiomorphWindowSize * 2);
 		// *4* Specify layout managers
 		mainFrame.setLayout(new BorderLayout()/*new BoxLayout(mainFrame,BoxLayout.Y_AXIS)*/);
@@ -102,7 +111,7 @@ public class GraphicsMain
 	}
 	private void exitApp()
 	{
-		int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		int response = JOptionPane.showConfirmDialog(mainFrame, "Are you sure that you want to quit?", "Quit?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 		if (response == JOptionPane.YES_OPTION)
 		{
 			System.exit(0);
@@ -110,7 +119,6 @@ public class GraphicsMain
 	}
 	public static void main(String[] args)
 	{
-		@SuppressWarnings("unused")
-		GraphicsMain gm = new GraphicsMain();
+		new GraphicsMain();
 	}
 }
