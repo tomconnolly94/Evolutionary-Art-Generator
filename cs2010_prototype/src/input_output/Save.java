f207baf4a2bab41f9f9dea8df4c73105703c1dea
Tom Connolly <tom.connolly2511@gmail.com>
2015-04-30 16:29:50 +0100

    got rid of save to image class.

--- cs2010_prototype/src/input_output/Save.java
+++ cs2010_prototype/src/input_output/Save.java
@@ -1,13 +1,9 @@
 package input_output;
 import genes.Gene;
 import gui.OpenGLFrame;
-import java.awt.AWTException;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Robot;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -19,17 +15,13 @@ import java.nio.ByteBuffer;
 import javax.imageio.ImageIO;
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
-import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
-import javax.media.opengl.GLContext;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.awt.GLCanvas;
-import javax.media.opengl.awt.GLJPanel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.text.View;
-import org.lwjgl.BufferUtils;
 import jogamp.opengl.glu.mipmap.Image;
+import org.lwjgl.BufferUtils;
 import biomorphHandling.*;
 import com.jogamp.opengl.util.FPSAnimator;
 /**
@@ -41,56 +33,55 @@ public class Save
 {
 	static FileOutputStream fop = null;
 	static File file;
-	private String fileDest = "C:/Users/Tom/Pictures/biomorphImages/biomorphImage.png";
-	
-	public Save(GLCanvas canvas) throws AWTException
+
+	public Save(Gene[] geneValues, String fileName)
 	{
-		BufferedImage screenshot = new BufferedImage(canvas.getWidth(), canvas.getHeight(), BufferedImage.TYPE_INT_RGB);
-	    Graphics graphics = screenshot.getGraphics();
-
-	    ByteBuffer buffer = BufferUtils.createByteBuffer(canvas.getWidth() * canvas.getHeight() * 3);
-
-	    glReadPixels(0, 0, canvas.getWidth(), canvas.getHeight(), GL2.GL_RGB, GL2.GL_UNSIGNED_BYTE, buffer);
-
-
-	    for (int h = 0; h < canvas.getHeight(); h++) {
-	        for (int w = 0; w < canvas.getWidth(); w++) {
-	            graphics.setColor(new Color( buffer.get()*2, buffer.get()*2, buffer.get()*2 ));
-	            graphics.drawRect(w,canvas.getHeight() - h, 1, 1); // canvas.getHeight() - h is for flipping the image
-	        }
-	    }
-	    try {
-            ImageIO.write(screenshot, "PNG", new File(fileDest));
-        } catch (IOException ex) {
-        }
+		try
+		{
+			String content = "";
+			for (Gene gene : geneValues)
+			{
+				content = content + gene.getValue() + ", ";
+			}
+			// save file to src
+			file = new File("src/" + fileName + ".txt");
+			fop = new FileOutputStream(file);
+			// if file doesnt exists, then create it
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+			// get the content in bytes
+			byte[] contentInBytes = content.getBytes();
+			fop.write(contentInBytes);
+			fop.flush();
+			fop.close();
+			System.out.println("Genes have been saved.");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if (fop != null)
+				{
+					fop.close();
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	public static void main(String[] args) throws AWTException{
-		GLCanvas canvas = new GLCanvas(new GLCapabilities(GLProfile.getDefault()));
+	public static void main(String[] args){
 		BiomorphCreator bc = new BiomorphCreator();
-		OpenGLFrame oframe = new OpenGLFrame(bc.createBiomorph(),400);
-		canvas.addGLEventListener(oframe);
-		canvas.addKeyListener(oframe);
-		int width = 400;
-		int height = 400;
-		JFrame frame = new JFrame("FRAME");
-		JPanel panel = new JPanel();
-		panel.setSize(width, height);
-		frame.setSize(width, height);
-		frame.add(canvas);
-		frame.setVisible(true);
-		frame.addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent e)
-			{
-				System.exit(0);
-			}
-		});
-		frame.add(panel);
-		FPSAnimator animator = new FPSAnimator(canvas, 60);
-		animator.start();
-		
-		Save save = new Save(canvas);
+		Biomorph biomorph = bc.createBiomorph();
+		Save save = new Save(biomorph.getGenes(), "biomorph");
 	}
 	
 	
