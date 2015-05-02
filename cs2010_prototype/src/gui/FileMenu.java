82a883f8b65a575e4384fa499b4c526789cffe16
dmd1337 <dmd1337@hotmail.co.uk>
2015-05-02 14:42:30 +0100

    File Menu changes

--- cs2010_prototype/src/gui/FileMenu.java
+++ cs2010_prototype/src/gui/FileMenu.java
@@ -10,6 +10,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.ToolTipManager;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import biomorphHandling.Biomorph;
@@ -18,19 +20,18 @@ import biomorphHandling.BiomorphManager;
  * The File Menu element of the program
  * @author Charandeep Rai
  */
-public class FileMenu extends JComponent implements MenuListener, ActionListener {
-
-	/**
-	 * 
-	 */
+public class FileMenu extends JComponent implements MenuListener, ActionListener
+{
 	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private Biomorph biomorph;
 	private BiomorphManager bm;
 	private GraphicsMain gm;
-	
 	public FileMenu(BiomorphManager bm, GraphicsMain gm)
 	{
+		//These two methods make the file menu "heavyweight" so it draws on top of the canvas.
+		JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+		ToolTipManager.sharedInstance().setLightWeightPopupEnabled(false);
 		this.gm = gm;
 		this.bm = bm;
 		initialise();
@@ -39,138 +40,44 @@ public class FileMenu extends JComponent implements MenuListener, ActionListener
 	{
 		return (JPanel)frame.getContentPane();
 	}
+	protected void initialise()
+	{
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(createMenuBar("Save", 1));
+		frame.pack();
+	} 
 	private JMenuBar createMenuBar(String name, int depth)
 	{
 		JMenuBar menubar = new JMenuBar();
-		menubar.add(createNewMenu("New", 4));
-		menubar.add(createSaveMenu("Save", 1));
-		menubar.add(createPrintMenu("Print", 3));
-		menubar.add(createOpenMenu("Open", 2));
-		
+		menubar.add(createBiomorphMenu("Biomorph", 4));
+		menubar.add(createHallOfFameMenu("Hall Of Fame", 3));
 		return menubar;
 	}
-	
-	public void updateBiomorph(Biomorph biomorph){
+	public void updateBiomorph(Biomorph biomorph)
+	{
 		this.biomorph = biomorph;
 	}
-	
-	private JMenu createSaveMenu(String savename, int menudepth)
+	private JMenu createBiomorphMenu(String name, int depth)
 	{
-		JMenu savemenu = new JMenu("Save");
-		JMenuItem Textfilesave = new JMenuItem("Save to Text File");
-		
-		
-		for (int i = 0; i < 1; i++)
-		{
-			savemenu.add(Textfilesave(Textfilesave));
-		}
-		return savemenu;
+		JMenu biomorphMenu = new JMenu("Biomorph");
+		biomorphMenu.add(createNewMenuItem("Create New Biomorph"));
+		biomorphMenu.add(createOpenMenuItem("Open from Text File"));
+		biomorphMenu.add(createSaveMenuItem("Save to Text File"));
+		biomorphMenu.add(createPrintMenuItem("Print Biomorph"));
+		return biomorphMenu;
 	}
-	private JMenu createOpenMenu(String name, int menudepth)
+	private JMenu createHallOfFameMenu(String name, int depth)
 	{
-		JMenu openmenu = new JMenu(name);
-		for (int i = 0; i < 1; i++)
-		{
-			openmenu.add(createOpenMenuItem("Open from TextFile"));
-		}
-		return openmenu;
+		JMenu biomorphMenu = new JMenu("Hall of Fame");
+		biomorphMenu.add(createAddToHallOfFameItem("Add to Hall of Fame"));
+		biomorphMenu.add(createRemoveSelectedItem("Remove Selected"));
+		biomorphMenu.add(createClearHallOfFameItem("Clear Hall of Fame"));
+		return biomorphMenu;
 	}
-	private JMenu createPrintMenu(String name, int menudepth)
-	{
-		JMenu printmenu = new JMenu(name);
-		for (int i = 0; i < 1; i++)
-		{
-			printmenu.add(createPrintMenuItem("Print Biomorph"));
-		}
-		return printmenu;
-	}
-	private JMenu createNewMenu(String name, int menudepth)
-	{
-		JMenu newmenu = new JMenu(name);
-		for (int i = 0; i < 1; i++)
-		{
-			newmenu.add(createNewMenuItem("Create New Biomorph"));
-		}
-		return newmenu;
-	}
-	private JMenuItem createOpenMenuItem(String name)
+	private JMenuItem createNewMenuItem(String name)
 	{
 		final JMenuItem jMenuItem = new JMenuItem(name);
-		jMenuItem.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-					String response = JOptionPane.showInputDialog(frame, "Please enter a file name:", null);
-				
-					Load load;
-					load = new Load(response);
-					Biomorph loadedBiomorph = load.load();
-					bm.addSpecific(loadedBiomorph);
-					gm.refreshMainPanel();				
-			}
-		});
-		return jMenuItem;
-	}
-	
-	private JMenuItem createSaveMenuItem(String savebutton)
-	{
-		
-		JMenuItem jMenuItem = new JMenuItem(savebutton);
-		jMenuItem.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent s)
-			{
-		}});
-		return jMenuItem;
-		
-	}
-	
-	private JMenuItem Textfilesave(JMenuItem Textfilesave){
-		
-		Textfilesave.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent arg0)
-			{
-				String response = JOptionPane.showInputDialog(frame, "Please enter a file name:", null);
-				
-				Save save = new Save(biomorph.getGenes(), response);
-			}
-		});
-		return Textfilesave;
-	}
-	
-	private JMenuItem loadBiomorph(JMenuItem loadItem){
-		
-		loadItem.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent arg0)
-			{
-				
-		}});
-		return loadItem;
-	}
-	
-	private JMenuItem Bioserialise(JMenuItem Bioserialise){
-		
-		Bioserialise.addActionListener(new ActionListener()
-		{
-
-			@Override
-			public void actionPerformed(ActionEvent arg0)
-			{
-				
-			}
-			
-		});
-		return Bioserialise;
-	}
-
-	private JMenuItem createNewMenuItem(String newbutton)
-	{
-		final JMenuItem jMenuItem = new JMenuItem(newbutton);
 		jMenuItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent n)
@@ -180,9 +87,39 @@ public class FileMenu extends JComponent implements MenuListener, ActionListener
 		});
 		return jMenuItem;
 	}
-	private JMenuItem createPrintMenuItem(String printbutton)
+	private JMenuItem createOpenMenuItem(String name)
 	{
-		final JMenuItem jMenuItem = new JMenuItem(printbutton);
+		final JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				String response = JOptionPane.showInputDialog(frame, "Please enter a file name:", null);
+				Load load;
+				load = new Load(response);
+				Biomorph loadedBiomorph = load.load();
+				bm.addSpecific(loadedBiomorph);
+				gm.refreshMainPanel();				
+			}
+		});
+		return jMenuItem;
+	}
+	private JMenuItem createSaveMenuItem(String name)
+	{	
+		JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				String response = JOptionPane.showInputDialog(frame, "Please enter a file name:", null);
+				Save save = new Save(biomorph.getGenes(), response);
+			}
+		});
+		return jMenuItem;
+	}
+	private JMenuItem createPrintMenuItem(String name)
+	{
+		final JMenuItem jMenuItem = new JMenuItem(name);
 		jMenuItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent n)
@@ -192,35 +129,125 @@ public class FileMenu extends JComponent implements MenuListener, ActionListener
 		});
 		return jMenuItem;
 	}
-	protected void initialise()
+	private JMenuItem createAddToHallOfFameItem(String name)
 	{
-		frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(createMenuBar("Save", 1));
-		frame.pack();
-	} 
-	
+		final JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent n)
+			{
+				JOptionPane.showMessageDialog(jMenuItem, "Added!");
+			}
+		});
+		return jMenuItem;
+	}
+	private JMenuItem createRemoveSelectedItem(String name)
+	{	
+		final JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent n)
+			{
+				JOptionPane.showMessageDialog(jMenuItem, "Removed!");
+			}
+		});
+		return jMenuItem;
+	}
+	private JMenuItem createClearHallOfFameItem(String name)
+	{
+		final JMenuItem jMenuItem = new JMenuItem(name);
+		jMenuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent n)
+			{
+				JOptionPane.showMessageDialog(jMenuItem, "Cleared!");
+			}
+		});
+		return jMenuItem;
+	}
+	public void updateBM(BiomorphManager bm)
+	{
+		this.bm = bm;
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
+	public void menuSelected(MenuEvent e)
+	{
+		
+	}
+	public void menuDeselected(MenuEvent e)
+	{
+		
+	}
+	public void menuCanceled(MenuEvent e)
+	{
+		
+	}
 	public static void main(String[] args)
 	{
 		GraphicsMain gm = new GraphicsMain();
 		BiomorphManager bm = new BiomorphManager();
 		new FileMenu(bm, gm).initialise();
 	}
-	
-	public void updateBM(BiomorphManager bm){
-		this.bm = bm;
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{}
-	@Override
-	public void menuCanceled(MenuEvent e)
-	{}
-	@Override
-	public void menuDeselected(MenuEvent e)
-	{}
-	@Override
-	public void menuSelected(MenuEvent e)
-	{}
+//	private JMenu createSaveMenu(String savename, int menudepth)
+//	{
+//		JMenu savemenu = new JMenu("Save");
+//		JMenuItem Textfilesave = new JMenuItem("Save to Text File");
+//		for (int i = 0; i < 1; i++)
+//		{
+//			savemenu.add(Textfilesave(Textfilesave));
+//		}
+//		return savemenu;
+//	}
+//	private JMenu createOpenMenu(String name, int menudepth)
+//	{
+//		JMenu openmenu = new JMenu(name);
+//		for (int i = 0; i < 1; i++)
+//		{
+//			openmenu.add(createOpenMenuItem("Open from TextFile"));
+//		}
+//		return openmenu;
+//	}
+//	private JMenu createPrintMenu(String name, int menudepth)
+//	{
+//		JMenu printmenu = new JMenu(name);
+//		for (int i = 0; i < 1; i++)
+//		{
+//			printmenu.add(createPrintMenuItem("Print Biomorph"));
+//		}
+//		return printmenu;
+//	}
+//	private JMenu createNewMenu(String name, int menudepth)
+//	{
+//		JMenu newmenu = new JMenu(name);
+//		for (int i = 0; i < 1; i++)
+//		{
+//			newmenu.add(createNewMenuItem("Create New Biomorph"));
+//		}
+//		return newmenu;
+//	}
+//	private JMenuItem loadBiomorph(JMenuItem loadItem)
+//	{
+//		loadItem.addActionListener(new ActionListener()
+//		{
+//			@Override
+//			public void actionPerformed(ActionEvent arg0)
+//			{
+//			}
+//		});
+//		return loadItem;
+//	}
+//	private JMenuItem Bioserialise(JMenuItem Bioserialise)
+//	{
+//		Bioserialise.addActionListener(new ActionListener()
+//		{
+//			@Override
+//			public void actionPerformed(ActionEvent arg0)
+//			{
+//			}
+//		});
+//		return Bioserialise;
+//	}
 }
