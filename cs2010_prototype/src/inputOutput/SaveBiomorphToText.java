8a467d906e9d9e912ef9ac075d6bb2e5fcfc48e9
Tom Connolly <tom.connolly2511@gmail.com>
2015-05-06 13:16:22 +0100

    removed unused imports

--- cs2010_prototype/src/inputOutput/SaveBiomorphToText.java
+++ cs2010_prototype/src/inputOutput/SaveBiomorphToText.java
@@ -1,6 +1,5 @@
 package inputOutput;
 import genes.Gene;
-import genes.Length;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -59,11 +58,5 @@ public class SaveBiomorphToText
 			{
 			}
 		}
-		
-		
-	}
-	public static void main(String[] args){
-		Gene[] genes = {new Length(0,"b"),new Length(1,"b"),new Length(2,"b"),new Length(3,"b"),new Length(4,"b"),new Length(5,"b"),new Length(6,"b"),new Length(7,"b"),new Length(8,"b"),new Length(9,"b"),new Length(10,"b"),new Length(11,"b")};
-		SaveBiomorphToText save = new SaveBiomorphToText(genes);
 	}
 }
