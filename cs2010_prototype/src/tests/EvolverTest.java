f4a15927a1fa0a4918060866c2567d4d6bd04755
Tom Connolly <tom.connolly2511@gmail.com>
2015-05-07 12:33:08 +0100

    evolver test finished

--- cs2010_prototype/src/tests/EvolverTest.java
+++ cs2010_prototype/src/tests/EvolverTest.java
@@ -22,7 +22,19 @@ public class EvolverTest
 		Biomorph b3 = e.evolveClo();
 		Gene[] genes = b3.getGenes();
 		for(int i=0;i<genes.length;i++){
-			assertEquals(genes[i].getValue(),targetValues[i]);
+			//asserts that the child gene value is the same of the parent that is closest
+			//to the target values, in this case parent 1's genes are all closer so the values
+			//should all be 0.
+			assertEquals(genes[i].getValue(),0);
+		}
+			
+	}
+	public void testEvolveAv(){
+		Biomorph b3 = e.evolveAv();
+		Gene[] genes = b3.getGenes();
+		for(int i=0;i<genes.length;i++){
+			//the average of 0 and 10(parent Biomorphs gene values) is 5 so all the child genes should be 5
+			assertEquals(genes[i].getValue(),5);
 		}
 			
 	}
